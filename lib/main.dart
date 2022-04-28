bc8aaecb550cb940273fc71a8ab2d206d478861a
Alan <alan88w@yahoo.com>
2022-04-28 20:30:54 +0800

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -18,56 +18,38 @@ class _XylophoneAppState extends State<XylophoneApp> {
     player.play('note$s.wav');
   }
 
+  Expanded buildKey({Color color, int soundNumber}) {
+    return
+        Expanded(
+          child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: color,
+                      shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(0))),
+                    ),
+                    onPressed: () {
+                      playSound(soundNumber);
+                    },
+                ),
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.red, fixedSize: Size(150.0, 30.0),),
-                onPressed: () {
-                  playSound(1);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.orange, fixedSize: Size(150.0, 30.0),),
-                onPressed: () {
-                  playSound(2);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.yellow, fixedSize: Size(150.0, 30.0),),
-                onPressed: () {
-                  playSound(3);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.green, fixedSize: Size(150.0, 30.0),),
-                onPressed: () {
-                  playSound(4);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.teal, fixedSize: Size(150.0, 30.0),),
-                onPressed: () {
-                  playSound(5);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.blue, fixedSize: Size(150.0, 30.0),),
-                onPressed: () {
-                  playSound(6);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.purple, fixedSize: Size(150.0, 30.0),),
-                onPressed: () {
-                  playSound(7);
-                },
-              ),
-
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
             ],
           ),
           // child: Container(),
