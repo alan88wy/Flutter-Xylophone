f4e14fc2c68208ce1d76475858b236de384fb233
Alan <alan88w@yahoo.com>
2022-04-28 17:27:20 +0800

    Added code to play music

--- lib/main.dart
+++ lib/main.dart
@@ -12,24 +12,63 @@ class XylophoneApp extends StatefulWidget {
 
 class _XylophoneAppState extends State<XylophoneApp> {
 
-  var s = 3;
+  final AudioCache player = AudioCache();
+
+  void playSound(int s) {
+    player.play('note$s.wav');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final AudioCache player = AudioCache();
-                // AudioPlayer advancedPlayer = AudioPlayer();
+          child: Column(
+            children: [
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.red, fixedSize: Size(150.0, 30.0),),
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.orange, fixedSize: Size(150.0, 30.0),),
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.yellow, fixedSize: Size(150.0, 30.0),),
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.green, fixedSize: Size(150.0, 30.0),),
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.teal, fixedSize: Size(150.0, 30.0),),
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.blue, fixedSize: Size(150.0, 30.0),),
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.purple, fixedSize: Size(150.0, 30.0),),
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
 
-                player.play('note$s.wav');
-              },
-              child: Text("Click Me"),
-            ),
-
+            ],
           ),
           // child: Container(),
         ),
